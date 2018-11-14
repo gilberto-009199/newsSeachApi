f7af2fb638ad39282b388305b20b31b2ac8b1057
GILBERTO RAMOS DE OLIVEIRA <16254850@CFP127ALUNO.LOCAL>
2018-11-14 09:34:23 -0200

    CRIANDO A ESTRUTURA BASICA

---
+++ conf.php
@@ -0,0 +1,5 @@
+<?php
+$URL_BASE="http://newsapi.org/v2/";
+$KEY_ACCESS="7f60f5dad321471caecb51c22c355513";
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
-echo "Ola Mundo da apiu do search";
+require("conf.php");
+
+$fonte= $_GET['sources'];
+$rota = $_GET['rota'];
+
+$context = stream_context_create([
+    'http' => [
+        'ignore_errors' => true,
+        'method'        => $method,
+        'header'        => $headers
+    ]
+]);
+
+$resposta = file_get_contents("$URL_BASE$rota?sources=$fonte&apiKey=$KEY_ACCESS",false,$context);
+echo $resposta;
 
 ?>
